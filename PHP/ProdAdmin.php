f772c61823e0aaee7b173ed579199e04c44bd62b
Nicolas Rivas <nrivas2017@alu.uct.cl>
2017-11-24 18:53:49 -0300

    arreglo

--- PHP/ProdAdmin.php
+++ PHP/ProdAdmin.php
@@ -11,14 +11,15 @@
 		<?php
 			include("conex.inc");
 
-			$consulta = "SELECT ID_Producto, Nombre, Precio FROM producto";
+			$consulta = "SELECT ID_Producto, Nombre, Precio,Descripcion FROM producto";
 			$respuesta = mysqli_query($db, $consulta);
-			echo "<table class='table'><tr> <td><b>Nombre</b></td>  <td><b>Precio</b></td> <td></td> <td></td></tr>";
+			echo "<table class='table'><tr> <td><b>Nombre</b></td>  <td><b>Precio</b></td> <td><b>Descripcion</b></td> <td></td> <td></td></tr>";
 			while($fila=mysqli_fetch_object($respuesta))
 				echo "<tr><td>$fila->Nombre</td>
 					      <td>$fila->Precio</td>
-					      <td><a href='#' onclick='EliminaPed($fila->N_Pedido)'><span class='glyphicon glyphicon-trash'></span></a></td>
-					      <td><button class='btn btn-success' onclick='PedListo($fila->N_Pedido)'>LISTO</button></td></tr>";
+					      <td>$fila->Descripcion</td>
+					      <td><a href='#' onclick='AjaxElimina($fila->ID_Producto)'><span class='glyphicon glyphicon-trash'></span></a></td>
+					      <td><a href='#' onclick='AjaxEdita($fila->ID_Producto)'><span class='glyphicon glyphicon-pencil'></span></a></td></tr>";
 			echo "</table>";
 		?>
 		</div>
